a13e57f948d77db59f3784198cb489109144f8f3
yamazakiyuu <pplmtv@gmail.com>
2020-07-12 16:54:33 +0900

    初回

--- app/src/main/java/jp/techacademy/yuu/qa_app/MainActivity.kt
+++ app/src/main/java/jp/techacademy/yuu/qa_app/MainActivity.kt
@@ -14,9 +14,13 @@ import android.view.MenuItem
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import android.support.design.widget.Snackbar
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import android.util.Base64
 import android.widget.ListView
-import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
@@ -87,7 +91,7 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
                             val answerBody = temp["body"] ?: ""
                             val answerName = temp["name"] ?: ""
                             val answerUid = temp["uid"] ?: ""
-                            val answer = Answer(answerBody, answerName, ansewerUid, key)
+                            val answer = Answer(answerBody, answerName, answerUid, key)
                             question.answers.add(answer)
                         }
                     }
@@ -108,91 +112,91 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
         override fun onCancelled(p0: DatabaseError) {
 
         }
+    }
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            mToolbar = findViewById(R.id.toolbar)
-            setSupportActionBar(mToolbar)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mToolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(mToolbar)
 
-            val fab = findViewById<FloatingActionButton>(R.id.fab)
-            fab.setOnClickListener { view ->
-                // ジャンルを選択していない場合(mGenre == 0)はエラーを表示するだけ
-                if (mGenre == 0) {
-                    Snackbar.make(view, "ジャンルを選択して下さい", Snackbar.LENGTH_LONG).show()
-                } else {
-
-                }
-
-                // ログイン済みのユーザーを取得する
-                val user = FirebaseAuth.getInstance().currentUser
-
-                // ログインしていなければログイン画面に遷移する
-                if (user == null) {
-                    val intent = Intent(applicationContext, LoginActivity::class.java)
-                    startActivity(intent)
-                } else {
-                    // ジャンルを渡して質問画像を起動する
-                    val intent = Intent(applicationContext, QuestionSendActivity::class.java)
-                    intent.putExtra("genre", mGenre)
-                    startActivity(intent)
-                }
-            }
-
-            // ナビゲーションドロワーの設定
-            val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
-            val toggle =
-                ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name)
-            drawer.addDrawerListener(toggle)
-            toggle.syncState()
-
-            val navigationView = findViewById<NavigationView>(R.id.nav_view)
-
-            // 1:趣味の規定を選択とする
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener { view ->
+            // ジャンルを選択していない場合(mGenre == 0)はエラーを表示するだけ
             if (mGenre == 0) {
-                onNavigationItemSelected(navigationView.menu.getItem(0))
+                Snackbar.make(view, "ジャンルを選択して下さい", Snackbar.LENGTH_LONG).show()
+            } else {
+
             }
 
-        }
+            // ログイン済みのユーザーを取得する
+            val user = FirebaseAuth.getInstance().currentUser
 
-        override fun onCreateOptionsMenu(menu: Menu): Boolean {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            menuInflater.inflate(R.menu.menu_main, menu)
-            return true
-        }
-
-        override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
-            val id = item.itemId
-
-            if (id == R.id.action_settings) {
-                val intent = Intent(applicationContext, SettingActivity::class.java)
+            // ログインしていなければログイン画面に遷移する
+            if (user == null) {
+                val intent = Intent(applicationContext, LoginActivity::class.java)
                 startActivity(intent)
-                return true
+            } else {
+                // ジャンルを渡して質問画像を起動する
+                val intent = Intent(applicationContext, QuestionSendActivity::class.java)
+                intent.putExtra("genre", mGenre)
+                startActivity(intent)
             }
-
-            return super.onOptionsItemSelected(item)
         }
 
-        override fun onNavigationItemSelected(item: MenuItem): Boolean {
-            val id = item.itemId
+        // ナビゲーションドロワーの設定
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val toggle =
+            ActionBarDrawerToggle(this, drawer, mToolbar, R.string.app_name, R.string.app_name)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
 
-            if (id == R.id.nav_hobby) {
-                mToolbar.title = "趣味"
-                mGenre = 1
-            } else if (id == R.id.nav_life) {
-                mToolbar.title = "生活"
-                mGenre = 2
-            } else if (id == R.id.nav_health) {
-                mToolbar.title = "健康"
-            } else if (id == R.id.nav_computer) {
-                mToolbar.title = "コンピューター"
-                mGenre = 4
-            }
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
 
-            val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
-            drawer.closeDrawer(GravityCompat.START)
+        // 1:趣味の規定を選択とする
+        if (mGenre == 0) {
+            onNavigationItemSelected(navigationView.menu.getItem(0))
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        val id = item.itemId
+
+        if (id == R.id.action_settings) {
+            val intent = Intent(applicationContext, SettingActivity::class.java)
+            startActivity(intent)
             return true
         }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == R.id.nav_hobby) {
+            mToolbar.title = "趣味"
+            mGenre = 1
+        } else if (id == R.id.nav_life) {
+            mToolbar.title = "生活"
+            mGenre = 2
+        } else if (id == R.id.nav_health) {
+            mToolbar.title = "健康"
+        } else if (id == R.id.nav_computer) {
+            mToolbar.title = "コンピューター"
+            mGenre = 4
+        }
+
+        val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
